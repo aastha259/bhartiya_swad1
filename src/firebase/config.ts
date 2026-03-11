6b9eb5048f2be0a9f3e45fd499a8125b37507551
Aastha Porwal <aasthaporwal47@gmail.com>
2026-03-11 14:59:07 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3747808795-e1fe5",
+  "appId": "1:832259040213:web:996b2a23a2462d9eae135c",
+  "apiKey": "AIzaSyBPfjHSlK6-BDa1_GickhXA68f6CWyRD-o",
+  "authDomain": "studio-3747808795-e1fe5.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "832259040213"
+};
